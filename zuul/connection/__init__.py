aacaf1bfc48f582e83ae51b39fed537374db2819
Tobias Henkel <tobias.henkel@bmw.de>
2021-02-22 17:48:28 +0100

    Protect connection event forwarder with timeout
    Change-Id: I9762a54e8f446e586e47cfafd099e5ee06b718ae

--- zuul/connection/__init__.py
+++ zuul/connection/__init__.py
@@ -703,8 +703,8 @@ class BaseThreadPoolEventConnector:
                 if not self._event_forward_queue[0].done():
                     return
                 future = self._event_forward_queue.popleft()
-                events, connection_event = future.result()
                 try:
+                    events, connection_event = future.result(timeout=900)
                     for event in events:
                         self.connection.logEvent(event)
                         if isinstance(event, model.DequeueEvent):
