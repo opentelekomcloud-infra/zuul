f388881f457f99092531e286be0bf29a993ffd6d
Benjamin Schanzel <benjamin.schanzel@bmw.de>
2025-01-29 14:55:54 +0100

    web: Upgrade nodejs to latest v23
    We currently use different outdated nodejs versions at various places.
    This change is aligning them where possible and upgrades to latest v23.
    
    Using node >= v18 requires us to set the --openssl-legacy-provider
    nodejs flag to keep compatiblity with our current js dependencies (ie.
    webpack). Uprading the js dependencies would be separate follow-up task.
    
    Depends-On: https://review.opendev.org/c/zuul/zuul-jobs/+/940373
    Change-Id: I6708675aa14e672b0a8a15f9a7ba3c610b42a903

--- tools/install-js-repos-rpm.sh
+++ tools/install-js-repos-rpm.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Copyright (c) 2016 NodeSource LLC
+# Copyright (c) 2024 NodeSource LLC
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -25,340 +25,139 @@
 # Discussion, issues and change requests at:
 #   https://github.com/nodesource/distributions
 #
-# Script to install the NodeSource Node.js 10.x repo onto an
+# Script to install the NodeSource Node.js 23.x repo onto an
 # Enterprise Linux or Fedora Core based system.
 #
-# This was downloaded from https://rpm.nodesource.com/setup_10.x
+# This was downloaded from https://rpm.nodesource.com/setup_23.x
 # A few modifications have been made.
 
-SCRSUFFIX="_10.x"
-NODENAME="Node.js 10.x"
-NODEREPO="pub_10.x"
-NODEPKG="nodejs"
+# Logger Function
+log() {
+  local message="$1"
+  local type="$2"
+  local timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+  local color
+  local endcolor="\033[0m"
 
-print_status() {
-  local outp=$(echo "$1") # | sed -r 's/\\n/\\n## /mg')
-  echo
-  echo -e "## ${outp}"
-  echo
+  case "$type" in
+    "info") color="\033[38;5;79m" ;;
+    "success") color="\033[1;32m" ;;
+    "error") color="\033[1;31m" ;;
+    *) color="\033[1;34m" ;;
+  esac
+
+  echo -e "${color}${timestamp} - ${message}${endcolor}"
 }
 
-if test -t 1; then # if terminal
-    ncolors=$(which tput > /dev/null && tput colors) # supports color
-    if test -n "$ncolors" && test $ncolors -ge 8; then
-        termcols=$(tput cols)
-        bold="$(tput bold)"
-        underline="$(tput smul)"
-        standout="$(tput smso)"
-        normal="$(tput sgr0)"
-        black="$(tput setaf 0)"
-        red="$(tput setaf 1)"
-        green="$(tput setaf 2)"
-        yellow="$(tput setaf 3)"
-        blue="$(tput setaf 4)"
-        magenta="$(tput setaf 5)"
-        cyan="$(tput setaf 6)"
-        white="$(tput setaf 7)"
-    fi
+# Error handler function
+handle_error() {
+  local exit_code=$1
+  local error_message="$2"
+  log "Error: $error_message (Exit Code: $exit_code)" "error"
+  exit $exit_code
+}
+
+# Function to check for command availability
+command_exists() {
+  command -v "$1" &> /dev/null
+}
+
+# Check if we are on an RPM-based system
+if ! [ -f /etc/redhat-release ] && ! grep -q "Amazon Linux" /etc/system-release 2>/dev/null; then
+    handle_error 1 "This script is intended for RPM-based systems. Please run it on an RPM-based system."
 fi
 
-print_bold() {
-    title="$1"
-    text="$2"
+log "Cleaning up old repositories..." "info"
+rm -f /etc/yum.repos.d/nodesource*.repo
+log "Old repositories removed" "info"
 
-    echo
-    echo "${red}================================================================================${normal}"
-    echo "${red}================================================================================${normal}"
-    echo
-    echo -e "  ${bold}${yellow}${title}${normal}"
-    echo
-    echo -en "  ${text}"
-    echo
-    echo "${red}================================================================================${normal}"
-    echo "${red}================================================================================${normal}"
-}
+# Define Node.js version
+NODE_VERSION="23.x"
 
-bail() {
-  echo 'Error executing command, exiting'
-  exit 1
-}
+# Get system architecture
+SYS_ARCH=$(uname -m)
 
-exec_cmd_nobail() {
-  echo "+ $1"
-  bash -c "$1"
-}
+# Validate system architecture
+case "$SYS_ARCH" in
+  aarch64|x86_64) log "Supported architecture: $SYS_ARCH" "info" ;;
+  *) handle_error 1 "Unsupported architecture: $SYS_ARCH. Only aarch64 and x86_64 are supported." ;;
+esac
 
-exec_cmd() {
-  exec_cmd_nobail "$1" || bail
-}
+# Repository content for Node.js
+NODEJS_REPO_CONTENT="[nodesource-nodejs]
+name=Node.js Packages for Linux RPM based distros - $SYS_ARCH
+baseurl=https://rpm.nodesource.com/pub_${NODE_VERSION}/nodistro/nodejs/$SYS_ARCH
+priority=9
+enabled=1
+gpgcheck=1
+gpgkey=https://rpm.nodesource.com/gpgkey/ns-operations-public.key
+module_hotfixes=1"
 
-node_deprecation_warning() {
-    if [[ "X${NODENAME}" == "Xio.js 1.x" ||
-          "X${NODENAME}" == "Xio.js 2.x" ||
-          "X${NODENAME}" == "Xio.js 3.x" ||
-          "X${NODENAME}" == "XNode.js 0.10" ||
-          "X${NODENAME}" == "XNode.js 0.12" ||
-          "X${NODENAME}" == "XNode.js 4.x LTS Argon" ||
-          "X${NODENAME}" == "XNode.js 5.x" ||
-          "X${NODENAME}" == "XNode.js 7.x" ]]; then
+# Write Node.js repository content
+echo "$NODEJS_REPO_CONTENT" | tee /etc/yum.repos.d/nodesource-nodejs.repo > /dev/null
 
-        print_bold \
-"                            DEPRECATION WARNING                            " "\
-${bold}${NODENAME} is no longer actively supported!${normal}
+# Check if Node.js version is an LTS version
+if [[ "$NODE_VERSION" == "18.x" ]] || [[ "$NODE_VERSION" == "20.x" ]]; then
+  # Repository content for N|Solid
+  NSOLID_REPO_CONTENT="[nodesource-nsolid]
+name=N|Solid Packages for Linux RPM based distros - $SYS_ARCH
+baseurl=https://rpm.nodesource.com/pub_${NODE_VERSION}/nodistro/nsolid/$SYS_ARCH
+priority=9
+enabled=1
+gpgcheck=1
+gpgkey=https://rpm.nodesource.com/gpgkey/ns-operations-public.key
+module_hotfixes=1"
 
-  ${bold}You will not receive security or critical stability updates${normal} for this version.
-
-  You should migrate to a supported version of Node.js as soon as possible.
-  Use the installation script that corresponds to the version of Node.js you
-  wish to install. e.g.
-
-   * ${green}https://deb.nodesource.com/setup_8.x — Node.js v8 LTS \"Carbon\"${normal} (recommended)
-   * ${green}https://deb.nodesource.com/setup_10.x — Node.js v10 Current${normal}
-
-  Please see ${bold}https://github.com/nodejs/Release${normal} for details about which
-  version may be appropriate for you.
-
-  The ${bold}NodeSource${normal} Node.js distributions repository contains
-  information both about supported versions of Node.js and supported Linux
-  distributions. To learn more about usage, see the repository:
-    ${bold}https://github.com/nodesource/distributions${normal}
-"
-        echo
-        echo "Continuing in 20 seconds ..."
-        echo
-        sleep 20
-    fi
-}
-
-script_deprecation_warning() {
-    if [ "X${SCRSUFFIX}" == "X" ]; then
-        print_bold \
-"                         SCRIPT DEPRECATION WARNING                         " "\
-This script, located at ${bold}https://rpm.nodesource.com/setup${normal}, used to
-  install Node.js v0.10, is deprecated and will eventually be made inactive.
-
-  You should use the script that corresponds to the version of Node.js you
-  wish to install. e.g.
-
-   * ${green}https://deb.nodesource.com/setup_8.x — Node.js v8 LTS \"Carbon\"${normal} (recommended)
-   * ${green}https://deb.nodesource.com/setup_10.x — Node.js v10 Current${normal}
-
-  Please see ${bold}https://github.com/nodejs/Release${normal} for details about which
-  version may be appropriate for you.
-
-  The ${bold}NodeSource${normal} Node.js Linux distributions GitHub repository contains
-  information about which versions of Node.js and which Linux distributions
-  are supported and how to use the install scripts.
-    ${bold}https://github.com/nodesource/distributions${normal}
-"
-
-        echo
-        echo "Continuing in 20 seconds (press Ctrl-C to abort) ..."
-        echo
-        sleep 20
-    fi
-}
-
-setup() {
-
-script_deprecation_warning
-node_deprecation_warning
-
-print_status "Installing the NodeSource ${NODENAME} repo..."
-
-print_status "Inspecting system..."
-
-if [ ! -x /bin/rpm ]; then
-  print_status """You don't appear to be running an Enterprise Linux based system,
-please contact NodeSource at https://github.com/nodesource/distributions/issues
-if you think this is incorrect or would like your distribution to be considered
-for support.
-"""
-  exit 1
+  # Write N|Solid repository content
+  echo "$NSOLID_REPO_CONTENT" | tee /etc/yum.repos.d/nodesource-nsolid.repo > /dev/null
+  log "Added N|Solid repository for LTS version: $NODE_VERSION" "info"
 fi
 
-## Annotated section for auto extraction in test.sh
-#-check-distro-#
+# Check for availability of dnf, yum or microdnf
+if command_exists dnf; then
+    log "dnf available, updating..." "info"
+    dnf makecache --disablerepo="*" --enablerepo="nodesource-nodejs"
+    
+    # Update N|Solid repository if it's LTS
+    if [[ "$NODE_VERSION" == "18.x" ]] || [[ "$NODE_VERSION" == "20.x" ]]; then
+        dnf makecache --disablerepo="*" --enablerepo="nodesource-nsolid"
+        log "Repository is configured and updated." "info"
+        log "You can use N|solid Runtime as a node.js alternative" "info"
+        log "To install N|solid Runtime, run: dnf install nsolid -y" "success"
+    else
+        log "Repository is configured and updated." "info"
+    fi
+    
+    log "Run 'dnf install nodejs -y' to complete the installation." "info"
+    exit 0
+elif command_exists yum; then
+    log "yum available, updating..." "info"
+    yum makecache --disablerepo="*" --enablerepo="nodesource-nodejs"
 
-## Check distro and arch
-echo "+ rpm -q --whatprovides redhat-release || rpm -q --whatprovides centos-release || rpm -q --whatprovides cloudlinux-release || rpm -q --whatprovides sl-release"
-DISTRO_PKG=$(rpm -q --whatprovides redhat-release || rpm -q --whatprovides centos-release || rpm -q --whatprovides cloudlinux-release || rpm -q --whatprovides sl-release)
-echo "+ uname -m"
-UNAME_ARCH=$(uname -m)
+    # Update N|Solid repository if it's LTS
+    if [[ "$NODE_VERSION" == "18.x" ]] || [[ "$NODE_VERSION" == "20.x" ]]; then
+        yum makecache --disablerepo="*" --enablerepo="nodesource-nsolid"
+        log "Repository is configured and updated." "info"
+        log "You can use N|solid Runtime as a node.js alternative" "info"
+        log "Run 'yum install nsolid -y' to complete the installation." "success"
+    else
+        log "Repository is configured and updated." "info"
+    fi
+    
+    log "Run 'yum install nodejs -y' to complete the installation." "info"
+elif command_exists microdnf; then
+    log "microdnf available, updating..." "info"
+    microdnf makecache --disablerepo="*" --enablerepo="nodesource-nodejs"
 
-
-if [ "X${UNAME_ARCH}" == "Xi686" ]; then
-  DIST_ARCH=i386
-elif [ "X${UNAME_ARCH}" == "Xx86_64" ]; then
-  DIST_ARCH=x86_64
+    # Update N|Solid repository if it's LTS
+    if [[ "$NODE_VERSION" == "18.x" ]] || [[ "$NODE_VERSION" == "20.x" ]]; then
+        microdnf makecache --disablerepo="*" --enablerepo="nodesource-nsolid"
+        log "Repository is configured and updated. Run 'microdnf install nsolid -y' to complete the installation." "info"
+    else
+        log "Repository is configured and updated." "info"
+    fi
+    
+    log "Run 'microdnf install nodejs -y' to complete the installation." "info"
 else
-
-  print_status "\
-You don't appear to be running a supported machine architecture: ${UNAME_ARCH}. \
-Please contact NodeSource at \
-https://github.com/nodesource/distributions/issues if you think this is \
-incorrect or would like your architecture to be considered for support. \
-"
-  exit 1
-
+    handle_error 1 "Neither yum, dnf nor microdnf package manager was found. Please update your system using your package manager."
 fi
-
-if [[ $DISTRO_PKG =~ ^(redhat|centos|cloudlinux|sl)- ]]; then
-    DIST_TYPE=el
-elif [[ $DISTRO_PKG =~ ^(enterprise|system)-release- ]]; then # Oracle Linux & Amazon Linux
-    DIST_TYPE=el
-elif [[ $DISTRO_PKG =~ ^(fedora|korora)- ]]; then
-    DIST_TYPE=fc
-else
-
-  print_status "\
-You don't appear to be running a supported version of Enterprise Linux. \
-Please contact NodeSource at \
-https://github.com/nodesource/distributions/issues if you think this is \
-incorrect or would like your architecture to be considered for support. \
-Include your 'distribution package' name: ${DISTRO_PKG}. \
-"
-  exit 1
-
-fi
-
-if [[ $DISTRO_PKG =~ ^system-release ]]; then
-
-  # Amazon Linux, for 2014.* use el7, older versions are unknown, perhaps el6
-  DIST_VERSION=7
-
-else
-
-  ## Using the redhat-release-server-X, centos-release-X, etc. pattern
-  ## extract the major version number of the distro
-  DIST_VERSION=$(echo $DISTRO_PKG | sed -r 's/^[[:alpha:]]+-release(-server|-workstation|-client)?-([0-9]+).*$/\2/')
-
-  if ! [[ $DIST_VERSION =~ ^[0-9][0-9]?$ ]]; then
-
-    print_status "\
-Could not determine your distribution version, you may not be running a \
-supported version of Enterprise Linux. \
-Please contact NodeSource at \
-https://github.com/nodesource/distributions/issues if you think this is \
-incorrect. Include your 'distribution package' name: ${DISTRO_PKG}. \
-"
-    exit 1
-
-  fi
-
-fi
-
-
-## Given the distro, version and arch, construct the url for
-## the appropriate nodesource-release package (it's noarch but
-## we include the arch in the directory tree anyway)
-RELEASE_URL_VERSION_STRING="${DIST_TYPE}${DIST_VERSION}"
-RELEASE_URL="\
-https://rpm.nodesource.com/${NODEREPO}/\
-${DIST_TYPE}/\
-${DIST_VERSION}/\
-${DIST_ARCH}/\
-nodesource-release-${RELEASE_URL_VERSION_STRING}-1.noarch.rpm"
-
-#-check-distro-#
-
-print_status "Confirming \"${DIST_TYPE}${DIST_VERSION}-${DIST_ARCH}\" is supported..."
-
-## Simple fetch & fast-fail to see if the nodesource-release
-## file exists for this distro/version/arch
-exec_cmd_nobail "curl -sLf -o /dev/null '${RELEASE_URL}'"
-RC=$?
-
-if [[ $RC != 0 ]]; then
-    print_status "\
-Your distribution, identified as \"${DISTRO_PKG}\", \
-is not currently supported, please contact NodeSource at \
-https://github.com/nodesource/distributions/issues \
-if you think this is incorrect or would like your distribution to be considered for support"
-    exit 1
-fi
-
-## EPEL is needed for EL5, we don't install it if it's missing but
-## we can give guidance
-if [ "$DIST_TYPE" == "el" ] && [ "$DIST_VERSION" == "5" ]; then
-
-  print_status "Checking if EPEL is enabled..."
-
-  echo "+ yum repolist enabled 2> /dev/null | grep epel"
-  repolist=$(yum repolist enabled 2> /dev/null | grep epel)
-
-  if [ "X${repolist}" == "X" ]; then
-    print_status "Finding current EPEL release RPM..."
-
-    ## We can scrape the html to find the latest epel-release (likely 5.4)
-    epel_url="http://dl.fedoraproject.org/pub/epel/5/${DIST_ARCH}/"
-    epel_release_view="${epel_url}repoview/epel-release.html"
-    echo "+ curl -s $epel_release_view | grep -oE 'epel-release-[0-9\-]+\.noarch\.rpm'"
-    epel=$(curl -s $epel_release_view | grep -oE 'epel-release-[0-9\-]+\.noarch\.rpm')
-    if [ "X${epel}" = "X" ]; then
-      print_status "Error: Could not find current EPEL release RPM!"
-      exit 1
-    fi
-
-    print_status """The EPEL (Extra Packages for Enterprise Linux) repository is a
-prerequisite for installing Node.js on your operating system. Please
-add it and re-run this setup script.
-
-The EPEL repository RPM is available at:
-  ${epel_url}${epel}
-You can try installing with: \`rpm -ivh <url>\`
-"""
-
-    exit 1
-  fi
-
-fi
-
-print_status "Downloading release setup RPM..."
-
-## Two-step process to install the nodesource-release RPM,
-## Download to a tmp file then install it directly with `rpm`.
-## We don't rely on RPM's ability to fetch from HTTPS directly
-echo "+ mktemp"
-RPM_TMP=$(mktemp || bail)
-
-exec_cmd "curl -sL -o '${RPM_TMP}' '${RELEASE_URL}'"
-
-print_status "Installing release setup RPM..."
-
-## --nosignature because nodesource-release contains the signature!
-exec_cmd "rpm -i --nosignature --force '${RPM_TMP}'"
-
-print_status "Cleaning up..."
-
-exec_cmd "rm -f '${RPM_TMP}'"
-
-print_status "Checking for existing installations..."
-
-## Nasty consequences if you have an existing Node or npm package
-## installed, need to inform if they are there
-echo "+ rpm -qa 'node|npm' | grep -v nodesource"
-EXISTING_NODE=$(rpm -qa 'node|npm|iojs' | grep -v nodesource)
-
-if [ "X${EXISTING_NODE}" != "X" ]; then
-
-  print_status """Your system appears to already have Node.js installed from an alternative source.
-Run \`${bold}sudo yum remove -y ${NODEPKG} npm${normal}\` to remove these first.
-"""
-
-fi
-
-print_status """Run \`${bold}sudo yum install -y ${NODEPKG}${normal}\` to install ${NODENAME} and npm.
-## You may also need development tools to build native addons:
-     sudo yum install gcc-c++ make
-## To install the Yarn package manager, run:
-     curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
-     sudo yum install yarn
-"""
-
-exit 0
-
-}
-
-## Defer setup until we have the complete script
-setup
