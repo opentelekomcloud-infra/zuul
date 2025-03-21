a81048b28ad9adae122da2d22ccfb6331019ea75
Axel Andersson <axel.andersson@volvocars.com>
2025-03-21 15:16:54 +0100

    Allow log output from FQCN tasks
    Allow FQCN (e.g. 'ansible.builtin.command' instead of just 'command')
    tasks to be readable in log.
    
    Change-Id: I751cdbd80cf061468ead76f990232dce70e16db0

--- zuul/ansible/base/callback/zuul_stream.py
+++ zuul/ansible/base/callback/zuul_stream.py
@@ -554,13 +554,17 @@ class CallbackModule(default.CallbackModule):
 
         if not is_localhost and is_task:
             self._stop_streamers()
-        if result._task.action in ('command', 'shell',
-                                   'win_command', 'win_shell'):
+        if result._task.action in (
+                'command', 'ansible.builtin.command',
+                'shell', 'ansible.builtin.shell',
+                'win_command', 'ansible.windows.win_command',
+                'win_shell', 'ansible.windows.win_shell'):
             stdout_lines = zuul_filter_result(result_dict)
             # We don't have streaming for localhost and windows modules so get
             # standard out after the fact.
             if is_localhost or result._task.action in (
-                    'win_command', 'win_shell'):
+                    'win_command', 'ansible.windows.win_command',
+                    'win_shell', 'ansible.windows.win_shell'):
                 for line in stdout_lines:
                     hostname = self._get_hostname(result)
                     self._log("%s | %s " % (hostname, line))
@@ -744,8 +748,11 @@ class CallbackModule(default.CallbackModule):
 
         if to_text(result_dict.get('msg', '')).startswith('MODULE FAILURE'):
             self._log_module_failure(result, result_dict)
-        elif result._task.action not in ('command', 'shell',
-                                         'win_command', 'win_shell'):
+        elif result._task.action not in (
+                'command', 'ansible.builtin.command',
+                'shell', 'ansible.builtin.shell',
+                'win_command', 'ansible.windows.win_command',
+                'win_shell', 'ansible.windows.win_shell'):
             if 'msg' in result_dict:
                 self._log_message(
                     result=result, msg=result_dict['msg'], status=status)
@@ -787,8 +794,11 @@ class CallbackModule(default.CallbackModule):
 
         if to_text(result_dict.get('msg', '')).startswith('MODULE FAILURE'):
             self._log_module_failure(result, result_dict)
-        elif result._task.action not in ('command', 'shell',
-                                         'win_command', 'win_shell'):
+        elif result._task.action not in (
+                'command', 'ansible.builtin.command',
+                'shell', 'ansible.builtin.shell',
+                'win_command', 'ansible.windows.win_command',
+                'win_shell', 'ansible.windows.win_shell'):
             self._log_message(
                 result=result,
                 msg="Item: {loop_var}".format(loop_var=result_dict[loop_var]),
