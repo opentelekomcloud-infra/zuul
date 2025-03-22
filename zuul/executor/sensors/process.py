57cad975e42e59b364c997faee942ed44180c98c
Clark Boylan <clark@acmegating.com>
2025-03-22 10:39:27 -0700

    Add a process limit governor to the executor
    The zuul executor has a number of governors that check system limits
    which will result in the executor unregistering itself from performing
    future work when near to these limits. One such limit is the number of
    processes that is allowed by the runtime system. Add a governor that
    will unregister the executor when near to the process limit.
    
    To do this we rely on cgroups' pids.max and pids.current values. There
    are two ways these manifest. The first is running within a container. In
    this case the values will be present in the root cgroup directory. When
    not running a container these values are not present. We could then
    attempt to rely on ulimits and process listings, but it appears that
    systemd creates a user.slice cgroup for each user and tracks the
    pids.max and pids.current (and on my system these match my ulimits) so
    we use this instead. Systemd is fairly ubiquitous and this prevents us
    from having two different implementations in place.
    
    This is important because running zuul jobs requires a number of
    processes. When we are near the process limit starting a new job will
    fork ansible and ansible forks ssh which also relies on control
    persistence processes and an ssh-agent. Rather than failing the job due
    to an inability to fork we should stop running new jobs until we have
    backed away from the limit.
    
    Change-Id: Ie24e386680087d7640fac13ceb34b1eb934d5146

---
+++ zuul/executor/sensors/process.py
@@ -0,0 +1,99 @@
+# Copyright 2025 Acme Gating, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+import math
+import os
+import os.path
+
+from zuul.executor.sensors import SensorInterface
+
+
+class ProcessSensor(SensorInterface):
+    log = logging.getLogger("zuul.executor.sensor.process")
+
+    def __init__(self, statsd, base_key, config=None):
+        super().__init__(statsd, base_key)
+        # The executor and ansible require a number of processes to function
+        # minimally: the executor itself, ansible, ssh control persistence,
+        # ssh and so on. Set a minimum of room for 10 processes before we
+        # stop.
+        self._min_headroom = 10
+        self._safety_factor = 0.05
+        self._uid = os.getuid()
+        self._root_cgroup_max_file = '/sys/fs/cgroup/pids.max'
+        self._root_cgroup_cur_file = '/sys/fs/cgroup/pids.current'
+        # This appears to be systemd specific behavior with cgroups that
+        # reflects the ulimit values. This way we don't need to have a
+        # separate system for ulimit checking.
+        self._user_cgroup_max_file = f'/sys/fs/cgroup/user.slice' \
+                                     f'/user-{self._uid}.slice/pids.max'
+        self._user_cgroup_cur_file = f'/sys/fs/cgroup/user.slice' \
+                                     f'/user-{self._uid}.slice/pids.current'
+
+    def isOk(self):
+        # Processes running in the root cgroup won't have these values
+        # but containers do.
+        # If no max is found assume infinity. If no current usage is found
+        # assume 1 for the current process.
+        root_max = self._get_root_cgroup_max() or math.inf
+        root_current = self._get_root_cgroup_current() or 1
+        # Processes running under systemd will have these values.
+        user_max = self._get_user_slice_max() or math.inf
+        user_current = self._get_user_slice_current() or 1
+
+        limit = min(root_max, user_max)
+        usage = max(root_current, user_current)
+        min_headroom = limit * self._safety_factor
+        if min_headroom < self._min_headroom:
+            min_headroom = self._min_headroom
+        # This shouldn't ever be negative but I'm not sure if you can reduce
+        # cgroup limits below the current usage at runtime.
+        headroom = max(limit - usage, 0)
+
+        if self.statsd:
+            # TODO(clarkb) can graphite/statsd handle math.inf?
+            self.statsd.gauge(self.base_key + '.max_process',
+                              limit)
+            self.statsd.gauge(self.base_key + '.cur_process',
+                              usage)
+
+        # When headroom is infinite we are never using too many processes.
+        # Also the math gets weird so just short circuit.
+        if headroom != math.inf and min_headroom >= headroom:
+            return False, f'high process utilization: {usage} max: {limit}'
+        return True, f'process utilization: {usage} max: {limit}'
+
+    def _get_root_cgroup_max(self):
+        return self._get_cgroup_value(self._root_cgroup_max_file)
+
+    def _get_root_cgroup_current(self):
+        return self._get_cgroup_value(self._root_cgroup_cur_file)
+
+    def _get_user_slice_max(self):
+        return self._get_cgroup_value(self._user_cgroup_max_file)
+
+    def _get_user_slice_current(self):
+        return self._get_cgroup_value(self._user_cgroup_cur_file)
+
+    def _get_cgroup_value(self, path):
+        if os.path.exists(path):
+            with open(path) as f:
+                s = f.read().strip()
+                if s == 'max':
+                    return math.inf
+                else:
+                    return int(s)
+        else:
+            return None
