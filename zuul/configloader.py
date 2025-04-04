blurrycontour/zuul-gerrit

Prepare oidc token for playbook execution in executor.
The OIDC secrets are decrypted togheter with normal secrets,
but the OIDC tokens are created right before the playbooks
execution if needed for those playbooks.

Specification: https://zuul-ci.org/docs/zuul/latest/developer/specs/zuul-workload-identity-federation.html

Change-Id: I6dcc0e807da86fd07deae0505d313c8297f1c096

zuul/configloader.py
@@ -43,11 +43,13 @@ from zuul.lib.varnames import check_varnames
 from zuul.zk.components import COMPONENT_REGISTRY
 from zuul.zk.semaphore import SemaphoreHandler
 from zuul.exceptions import (
+    AlgorithmNotSupportedException,
     CleanupRunDeprecation,
     DuplicateGroupError,
     DuplicateNodeError,
     GlobalSemaphoreNotFoundError,
     LabelForbiddenError,
+    MaxOIDCTTLError,
     MaxTimeoutError,
     MultipleProjectConfigurations,
     NodeFromGroupNotFoundError,
@@ -677,12 +679,21 @@ class SecretParser(object):
         self.pcontext = pcontext
         self.schema = self.getSchema()
 
+    def _check_missing_attribute(self, secret):
+        # It would be complex to use voluptuous validator to check
+        # "required and exclusive attributes", so we use custom validator.
+        if 'data' not in secret and 'oidc' not in secret:
+            raise vs.Invalid("Either 'data' or 'oidc' must be present.")
+        return secret
+
     def getSchema(self):
-        secret = {vs.Required('name'): str,
-                  vs.Required('data'): dict,
-                  '_source_context': model.SourceContext,
-                  '_start_mark': model.ZuulMark,
-                  }
+        secret = vs.All({
+            vs.Required('name'): str,
+            vs.Exclusive('data', 'secret_type'): dict,
+            vs.Exclusive('oidc', 'secret_type'): dict,
+            '_source_context': model.SourceContext,
+            '_start_mark': model.ZuulMark,
+        }, self._check_missing_attribute)
 
         return vs.Schema(secret)
 
@@ -692,7 +703,28 @@ class SecretParser(object):
         s = model.Secret(conf['name'], conf['_source_context'])
         s.source_context = conf['_source_context']
         s.start_mark = conf['_start_mark']
-        s.secret_data = conf['data']
+        if 'data' in conf:
+            s.secret_data = conf['data']
+        else:
+            tenant = self.pcontext.tenant
+            ttl = conf['oidc'].get('ttl')
+            if not ttl:
+                conf['oidc']['ttl'] = min(
+                    tenant.default_oidc_ttl,
+                    tenant.max_oidc_ttl)
+            elif int(ttl) > tenant.max_oidc_ttl:
+                raise MaxOIDCTTLError(s, tenant)
+
+            globals = self.pcontext.scheduler.globals
+            algorithm = conf['oidc'].get('algorithm')
+            if not algorithm:
+                conf['oidc']['algorithm'] = \
+                    globals.oidc_default_signing_algorithm
+            elif algorithm not in globals.oidc_supported_signing_algorithms:
+                raise AlgorithmNotSupportedException(
+                    f"Algorithm '{algorithm}' is not supported")
+
+            s.secret_oidc = conf['oidc']
         return s
 
 
@@ -2048,6 +2080,8 @@ class TenantParser(object):
             tenant.max_job_timeout = int(conf['max-job-timeout'])
         if conf.get('max-oidc-ttl') is not None:
             tenant.max_oidc_ttl = int(conf['max-oidc-ttl'])
+        if conf.get('default-oidc-ttl') is not None:
+            tenant.default_oidc_ttl = int(conf['default-oidc-ttl'])
         if conf.get('exclude-unprotected-branches') is not None:
             tenant.exclude_unprotected_branches = \
                 conf['exclude-unprotected-branches']

opentelekomcloud-infra/zuul

zuul/configloader.py
@@ -42,10 +42,12 @@ from zuul.lib.varnames import check_varnames
 from zuul.zk.components import COMPONENT_REGISTRY
 from zuul.zk.semaphore import SemaphoreHandler
 from zuul.exceptions import (
+    AlgorithmNotSupportedException,
     CleanupRunDeprecation,
     DuplicateGroupError,
     DuplicateNodeError,
     GlobalSemaphoreNotFoundError,
+    MaxOIDCTTLError,
     MultipleProjectConfigurations,
     NodeFromGroupNotFoundError,
     PipelineNotPermittedError,
@@ -638,12 +640,19 @@ class SecretParser(object):
         self.pcontext = pcontext
         self.schema = self.getSchema()
 
+    def _checkMissingAttribute(self, secret):
+        if 'data' not in secret and 'oidc' not in secret:
+            raise vs.Invalid("Either 'data' or 'oidc' must be present.")
+        return secret
+
     def getSchema(self):
-        secret = {vs.Required('name'): str,
-                  vs.Required('data'): dict,
-                  '_source_context': model.SourceContext,
-                  '_start_mark': model.ZuulMark,
-                  }
+        secret = vs.All({
+            vs.Required('name'): str,
+            vs.Exclusive('data', 'secret_type'): dict,
+            vs.Exclusive('oidc', 'secret_type'): dict,
+            '_source_context': model.SourceContext,
+            '_start_mark': model.ZuulMark,
+        }, self._checkMissingAttribute)
 
         return vs.Schema(secret)
 
@@ -652,7 +661,28 @@ class SecretParser(object):
         s = model.Secret(conf['name'], conf['_source_context'])
         s.source_context = conf['_source_context']
         s.start_mark = conf['_start_mark']
-        s.secret_data = conf['data']
+        if 'data' in conf:
+            s.secret_data = conf['data']
+        else:
+            tenant = self.pcontext.tenant
+            ttl = conf['oidc'].get('ttl')
+            if not ttl:
+                conf['oidc']['ttl'] = min(
+                    tenant.default_oidc_ttl,
+                    tenant.max_oidc_ttl)
+            elif int(ttl) > tenant.max_oidc_ttl:
+                raise MaxOIDCTTLError(s, tenant)
+
+            globals = self.pcontext.scheduler.globals
+            algorithm = conf['oidc'].get('algorithm')
+            if not algorithm:
+                conf['oidc']['algorithm'] = \
+                    globals.oidc_default_signing_algorithm
+            elif algorithm not in globals.oidc_supported_signing_algorithms:
+                raise AlgorithmNotSupportedException(
+                    f"Algorithm '{algorithm}' is not supported")
+
+            s.secret_oidc = conf['oidc']
         return s
 
 
@@ -1938,6 +1968,8 @@ class TenantParser(object):
             tenant.max_job_timeout = int(conf['max-job-timeout'])
         if conf.get('max-oidc-ttl') is not None:
             tenant.max_oidc_ttl = int(conf['max-oidc-ttl'])
+        if conf.get('default-oidc-ttl') is not None:
+            tenant.default_oidc_ttl = int(conf['default-oidc-ttl'])
         if conf.get('exclude-unprotected-branches') is not None:
             tenant.exclude_unprotected_branches = \
                 conf['exclude-unprotected-branches']