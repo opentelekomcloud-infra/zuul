c83f96113f5ffb27571f35cd9bd87b7ca4090a83
Tudor Tabacel <tudor-stefan.tabacel-manea@bmw.de>
2025-05-08 09:52:58 +0200

    mqttreporter: attach gerrit topic to the MQTT mesasge
    In order to better track the builds tied to a particular Gerrit topic,
    the MQTT message now also reports the topic attached to the change, if
    it exists.
    
    Change-Id: I25d39a255b4885d9bf4eda3ebcd004d2ba31f688

--- zuul/driver/mqtt/mqttreporter.py
+++ zuul/driver/mqtt/mqttreporter.py
@@ -49,6 +49,7 @@ class MQTTReporter(BaseReporter):
                 'commit_id': getattr(change, 'commit_id', ''),
                 'owner': getattr(change, 'owner', ''),
                 'ref': getattr(change, 'ref', ''),
+                'topic': getattr(change, 'topic', ''),
             }
             for change in item.changes
         ]
@@ -68,6 +69,7 @@ class MQTTReporter(BaseReporter):
             'commit_id': getattr(item.changes[0], 'commit_id', ''),
             'owner': getattr(item.changes[0], 'owner', ''),
             'ref': getattr(item.changes[0], 'ref', ''),
+            'topic': getattr(item.changes[0], 'topic', ''),
             'message': self._formatItemReport(
                 item, with_jobs=False),
             'trigger_time': item.event.timestamp,
