71f753705063f88ffba8649a87194e0d7c95f094
Simon Westphahl <simon.westphahl@bmw.de>
2025-07-14 10:58:39 +0200

    Add provider node lifetime span
    Span for the provider node that represents the full lifetime from
    request until the node is deleted.
    
    Change-Id: Ibcb08067820a48a9fd456ebda5f966a1a160b4c4

--- zuul/launcher/server.py
+++ zuul/launcher/server.py
@@ -1425,6 +1425,9 @@ class Launcher:
             self.system.system_id, label, node_uuid, provider, request)
         node_class = provider.driver.getProviderNodeClass()
         label_quota = provider.getQuotaForLabel(label)
+        with trace.use_span(request._span):
+            node_span = self.tracer.start_span("ProviderNode")
+        span_info = tracing.getSpanInfo(node_span)
         node = node_class.new(
             ctx,
             uuid=node_uuid,
@@ -1437,6 +1440,7 @@ class Launcher:
             executor_zone=label.executor_zone,
             request_id=request.uuid,
             zuul_event_id=request.zuul_event_id,
+            span_info=span_info,
             connection_name=provider.connection_name,
             tenant_name=request.tenant_name,
             provider=provider.canonical_name,
@@ -1738,6 +1742,8 @@ class Launcher:
                 log.debug("Removing provider node %s", node)
                 node.delete(ctx)
                 node.releaseLock(ctx)
+                tracing.endSavedSpan(
+                    node.span_info, attributes=node.getSpanAttributes())
 
     def _processMinReady(self):
         if not self.api.nodes_cache.waitForSync(
