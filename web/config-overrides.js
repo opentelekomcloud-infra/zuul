909b65d2e9f188cbbe6a05c13f0ac7b0a61a2fc3
Damian Fajfer <damian@fajfer.org>
2025-07-30 15:29:46 +0200

    Update React version to 17.0.0
    Similar approach to what was done 4 years ago in change 760458
    
    Change-Id: Ia9457947c0b7c46be15df474a56b023ccf8c48ac

--- web/config-overrides.js
+++ web/config-overrides.js
@@ -1,11 +1,40 @@
 const rewiredEsbuild = require("react-app-rewired-esbuild");
 
 module.exports = function override(config, env) {
-  // No additional config just want esbuild instead of babel
-  return rewiredEsbuild()(config, env);
+  // Use esbuild for our source code
+  config = rewiredEsbuild()(config, env);
+  
+  // Find the oneOf rule that contains the file type rules
+  const oneOfRule = config.module.rules.find(rule => rule.oneOf);
+  
+  if (oneOfRule) {
+    // Add a rule to handle Victory.js modules with Babel before esbuild
+    oneOfRule.oneOf.unshift({
+      test: /\.jsx?$/,
+      include: /node_modules\/(victory-|@patternfly\/react-charts)/,
+      exclude: /node_modules\/(?!(victory-|@patternfly\/react-charts))/,
+      use: {
+        loader: require.resolve('babel-loader'),
+        options: {
+          cacheDirectory: true,
+          cacheCompression: false,
+          presets: [
+            [require.resolve('@babel/preset-env'), { 
+              targets: { 
+                browsers: ["last 1 version", "> 1%", "maintained node versions", "not dead"] 
+              } 
+            }],
+            [require.resolve('@babel/preset-react'), { runtime: 'automatic' }]
+          ],
+          plugins: [
+            require.resolve('@babel/plugin-transform-class-properties'),
+            require.resolve('@babel/plugin-transform-optional-chaining'),
+            require.resolve('@babel/plugin-transform-nullish-coalescing-operator')
+          ]
+        }
+      }
+    });
+  }
+  
+  return config;
 };
-
-// use `customize-cra`
-const { override } = require("customize-cra");
-
-module.exports = override(rewiredEsbuild());
