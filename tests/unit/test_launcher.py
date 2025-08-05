10ac0bb00b1a8dca36a20e2b6104ef96696366cb
Szymon Datko <sdatko@redhat.com>
2025-08-05 16:15:22 +0200

    [dnm] Testing something
    Change-Id: Ie37251bc62643c362c3c5b23fda8d5c12fda217c

--- tests/unit/test_launcher.py
+++ tests/unit/test_launcher.py
@@ -1978,6 +1978,11 @@ class TestLauncherUpload(LauncherBaseTestCase):
             dict(name='build-ubuntu-local-image', result='SUCCESS'),
         ], ordered=False)
 
+        # for _ in iterate_timeout(30, ""):
+        #     if (self.scheds.first.sched.local_layout_state.get("tenant-one") ==
+        #         self.launcher.local_layout_state.get("tenant-one")):
+        #         break
+
         for name in [
                 'review.example.com%2Forg%2Fcommon-config/debian-local',
                 'review.example.com%2Forg%2Fcommon-config/ubuntu-local',
@@ -2032,6 +2037,8 @@ class TestLauncherUpload(LauncherBaseTestCase):
         artifacts = self._waitForArtifacts(image_cname, 2, format='raw')
         artifact_uuids = [x.uuid for x in artifacts]
         self.assertNotIn(oldest_artifact_uuid, artifact_uuids)
+
+        self.assertTrue(False)  # Force failure ~ for easy outputs comparison
 
 
 class TestMinReadyLauncher(LauncherBaseTestCase):
