b933e8285ac530354366de3e41a4b41cef984ea3
James E. Blair <jim@acmegating.com>
2025-08-12 11:23:25 -0700

    AWS: Remove security-group-id
    Remove this attribute in favor of security-group-ids.
    
    Change-Id: Id50ad2a0b08cc5900dcf3e16929d1de17b72fd0d

--- zuul/driver/aws/awsendpoint.py
+++ zuul/driver/aws/awsendpoint.py
@@ -1640,9 +1640,6 @@ class AwsProviderEndpoint(BaseProviderEndpoint):
 
             if label.security_group_ids:
                 template_data['SecurityGroupIds'] = label.security_group_ids
-            # TODO: remove backwards compat
-            elif label.security_group_id:
-                template_data['SecurityGroupIds'] = [label.security_group_id]
 
             if label.imds_http_tokens == 'required':
                 template_data['MetadataOptions'] = {
@@ -1861,11 +1858,6 @@ class AwsProviderEndpoint(BaseProviderEndpoint):
 
         if label.security_group_ids:
             args['NetworkInterfaces'][0]['Groups'] = label.security_group_ids
-        # TODO: remove backwards compat
-        elif label.security_group_id:
-            args['NetworkInterfaces'][0]['Groups'] = [
-                label.security_group_id
-            ]
 
         if label.subnet_ids:
             args['NetworkInterfaces'][0]['SubnetId'] = random.choice(
