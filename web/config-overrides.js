ebe7cbbe3184653f4d1fb3219ee6f12e845559c3
Damian Fajfer <damian@fajfer.org>
2025-08-18 12:29:30 +0200

    Update swagger-ui to 4.19.1
    Snyk reports three vulnerabilities in their database: https://security.snyk.io/package/npm/swagger-ui/3.25.3
    Upgrade fixes all of them, they're also identified as CVE-2018-25031 and CVE-2021-46708
    
    Change-Id: Iaf85ed3c621811752711c49fba3a6d0f0fa042e6

--- web/config-overrides.js
+++ web/config-overrides.js
@@ -4,15 +4,25 @@ module.exports = function override(config, env) {
   // Use esbuild for our source code
   config = rewiredEsbuild()(config, env);
 
+  // Force webpack to prefer CommonJS over ES modules for problematic packages
+  config.resolve.mainFields = ['browser', 'main', 'module'];
+
+  // Add aliases to force CommonJS versions of problematic packages
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    'swagger-client/es': 'swagger-client/lib',
+    '@swagger-api/apidom-reference/configuration/empty': '@swagger-api/apidom-reference/src/configuration/empty.cjs',
+  };
+
   // Find the oneOf rule that contains the file type rules
   const oneOfRule = config.module.rules.find(rule => rule.oneOf);
 
   if (oneOfRule) {
-    // Add a rule to handle Victory.js modules with Babel before esbuild
+    // Add a rule to handle Victory.js, swagger-ui, and ApiDOM modules with Babel before esbuild
     oneOfRule.oneOf.unshift({
       test: /\.jsx?$/,
-      include: /node_modules\/(victory-|@patternfly\/react-charts)/,
-      exclude: /node_modules\/(?!(victory-|@patternfly\/react-charts))/,
+      include: /node_modules\/(victory-|@patternfly\/react-charts|swagger-ui|swagger-client|@swagger-api)/,
+      exclude: /node_modules\/(?!(victory-|@patternfly\/react-charts|swagger-ui|swagger-client|@swagger-api))/,
       use: {
         loader: require.resolve('babel-loader'),
         options: {
