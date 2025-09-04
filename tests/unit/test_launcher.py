5388bb2123aae14595500e7fd30bfcb710c716a4
Simon Westphahl <simon.westphahl@bmw.de>
2025-09-04 13:24:10 +0200

    Fix provider quota calculation
    So far we've included the provider resource limits in the same quota
    information as the endpoint limits. However, this makes provider
    resource limits essentially global endpoint resources limits.
    
    This has the effect that a provider becomes unusable (and pauses) as
    soon as the unmanaged quota usage exceeds the provider resource limits.
    
    It looks like we have to distinguish two cases here in order to
    determine if a given provider has enough quota:
    
    1. check whether the provider endpoint has in principal enough resources
    2. we don't exceed the provider's configured resource limits
    
    For case 1. we need to consider unmanaged quota as well as quota used by
    OTHER providers using the same endpoint. The reason for this is, that
    there is no 1-1 mapping between an endpoint and a provider.
    
    To cover case 2. we need to use the lower value for a given quota key
    between the available endpoint quota (1. above) and the provider
    resource limits.
    
    Change-Id: Ifef8c94c64041c17c6fdf900caa0cfbdf8be1264

--- tests/unit/test_launcher.py
+++ tests/unit/test_launcher.py
@@ -1390,6 +1390,36 @@ class TestLauncher(LauncherBaseTestCase):
         with testtools.ExpectedException(Exception):
             self.requestNodes(["debian-normal"])
 
+    @simple_layout('layouts/resource-limits.yaml',
+                   enable_nodepool=True)
+    @driver_config('test_launcher', quotas={
+        'instances': 3,
+    })
+    @okay_tracebacks('_listImportSnapshotTasks')
+    def test_resource_limits_external_usage(self):
+        self.waitUntilSettled()
+
+        # Create 2 unmanaged instances that only leave one instance
+        # as available quota.
+        ec2_client = boto3.client('ec2', region_name='us-east-1')
+        ec2_client.run_instances(
+            ImageId="ami-12c6146b", MinCount=2, MaxCount=2,
+        )
+        self.launcher._provider_quota_cache.clear()
+
+        # The cleanup worker is responsible for recording external
+        # usage in the quota cache
+        self.launcher.cleanup_worker._run()
+
+        # We can still get a node since we've not reached
+        # the endpoint quota limit.
+        self.requestNodes(["debian-normal"])
+
+        # We expect a timeout waiting for nodes, as the
+        # endpoint has reached the instance limit.
+        with testtools.ExpectedException(Exception):
+            self.requestNodes(["debian-normal"])
+
     @simple_layout('layouts/nodepool.yaml',
                    enable_nodepool=True)
     @driver_config('test_launcher', quotas={
@@ -1741,9 +1771,9 @@ class TestLauncher(LauncherBaseTestCase):
         self.assertEqual(1, len(nodes0))
 
         # Make sure the next requests always have current quota info
-        self.launcher._provider_quota_cache = cachetools.TTLCache(
+        self.launcher._provider_limits_cache = cachetools.TTLCache(
             maxsize=8192, ttl=0)
-        self.launcher._provider_available_cache = cachetools.TTLCache(
+        self.launcher._provider_quota_cache = cachetools.TTLCache(
             maxsize=8192, ttl=0)
 
         requests = []
