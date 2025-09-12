36be91fed831d7c761cba49f411ca6a90ce6d768
James E. Blair <jim@acmegating.com>
2025-09-12 10:27:45 -0700

    Launcher: don't use the main node with slots
    The current slots implementation creates a "main" node as slot 0
    and subnodes as the remaining slots.  This change modifies the
    process to create subnodes for each slot, including 0, and then
    does not use the main node at all.
    
    This simplifies some things (like deciding if a node is actionable),
    and makes others more complex (like creating the node and subnodes,
    and assigning the subnode to the request).
    
    But the main benefit is that it lets us be more explicit about the
    lifecycle of the main and subnodes.  By not treating the main node
    as a subnode, we have more choices about what tags we associate with
    it and how we want it to appear to the user.  We also reduce the
    risk of accidentally relying (perhaps in a job) on something that
    only exists in the main node but not on a subnode.
    
    Change-Id: Ie79783a0824f44ce92a01b107097a52e5b253e99

--- zuul/model.py
+++ zuul/model.py
@@ -2780,7 +2780,7 @@ class ProviderNode(zkobject.PolymorphicZKObjectMixin,
         OUTDATED = "outdated"
         HOLD = "hold"
         SNAPSHOT = "snapshot"
-        PENDING_DELETE = "pending-delete"
+        SLOT_HOST = "slot-host"
 
     # States where quota counts
     ALLOCATED_STATES = (
@@ -2791,12 +2791,13 @@ class ProviderNode(zkobject.PolymorphicZKObjectMixin,
         State.OUTDATED,
         State.HOLD,
         State.SNAPSHOT,
-        State.PENDING_DELETE,
+        State.SLOT_HOST,
     )
 
     FINAL_STATES = (
         State.READY,
         State.FAILED,
+        State.SLOT_HOST,
     )
 
     FAILED_STATES = (
