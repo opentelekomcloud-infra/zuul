d6fc21efdce72f47d807b674acc4a814cfcf28b0
Benjamin Schanzel <benjamin.schanzel@bmw.de>
2025-11-04 13:29:15 +0100

    Don't log request token in log streamer
    Currently, we print access tokens from log streaming requests to the log
    output. Avoid this by taking the token out of the request object before
    logging.
    
    Change-Id: I1713e7eb9dfe1cdb2e33ae25e789e8c6cdc7dd76

--- zuul/web/__init__.py
+++ zuul/web/__init__.py
@@ -1123,12 +1123,16 @@ class LogStreamHandler(WebSocket):
     def received_message(self, message):
         if message.is_text:
             req = json.loads(message.data.decode('utf-8'))
+
+            token = None
+            if 'token' in req:
+                token = req.pop('token')
+
             self.log.debug("Websocket request: %s", req)
             if self.streamer:
                 self.log.debug("Ignoring request due to existing streamer")
                 return
 
-            token = req.get('token')
             try:
                 self.auth_context.validate(token=token)
             except APIError as e:
