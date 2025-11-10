e09393348cbad1d5bf63edadef279b525cb15cc3
Mohammed Naser <mnaser@vexxhost.com>
2025-11-10 13:27:29 -0500

    Upgrade Xterm.js to version 5.x
    Change-Id: Ie291bc38930c6b88da7389774a812311d4ebc2dd
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>

--- web/config-overrides.js
+++ web/config-overrides.js
@@ -1,11 +1,22 @@
 const rewiredEsbuild = require("react-app-rewired-esbuild");
 
 module.exports = function override(config, env) {
-  // No additional config just want esbuild instead of babel
-  return rewiredEsbuild()(config, env);
+  // Use esbuild to transpile the code
+  config = rewiredEsbuild()(config, env);
+
+  // Transpile Xterm.js since it ships UMD code which we cannot
+  // use directly in our ES6 modules.
+  config.module.rules.push({
+    test: /\.js$/,
+    include: /node_modules\/@xterm/,
+    use: {
+      loader: require.resolve('esbuild-loader'),
+      options: {
+        loader: 'js',
+        target: 'es2015',
+      },
+    },
+  });
+
+  return config;
 };
-
-// use `customize-cra`
-const { override } = require("customize-cra");
-
-module.exports = override(rewiredEsbuild());
