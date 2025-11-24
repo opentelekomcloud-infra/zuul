5e02c52cad1188462716448cc841824210b01dd9
Clark Boylan <clark@acmegating.com>
2025-11-23 18:05:50 -0800

    WIP Start sketching out implementation of web RBAC spec
    There are a number of pieces to sort out when implementing the RBAC spec
    from configuration loading to accurate application of rules on web
    requests. Start an early sketch of what this might look like so that we
    can identify any concerns with the approach before too much time is
    spent committing to it.
    
    Note this is completely non functional in this state, but I think it
    gives us something concrete enough to assess the approach.
    
    Depends-On: https://review.opendev.org/c/zuul/zuul/+/967938
    Change-Id: Ibaaabcba5334a26f6e14a030b02f0f926207e15f

--- zuul/model.py
+++ zuul/model.py
@@ -9672,6 +9672,7 @@ class UnparsedAbideConfig(object):
         self.ltime = -1
         self.tenants = {}
         self.authz_rules = []
+        self.authz_roles = []
         self.semaphores = []
         self.api_roots = []
 
@@ -9679,6 +9680,7 @@ class UnparsedAbideConfig(object):
         if isinstance(conf, UnparsedAbideConfig):
             self.tenants.update(conf.tenants)
             self.authz_rules.extend(conf.authz_rules)
+            self.authz_roles.extend(conf.authz_roles)
             self.semaphores.extend(conf.semaphores)
             self.api_roots.extend(conf.api_roots)
             if len(self.api_roots) > 1:
@@ -9704,6 +9706,8 @@ class UnparsedAbideConfig(object):
                 self.authz_rules.append(value)
             elif key == 'authorization-rule':
                 self.authz_rules.append(value)
+            elif key == 'role':
+                self.authz_roles.append(value)
             elif key == 'global-semaphore':
                 self.semaphores.append(value)
             elif key == 'api-root':
@@ -9721,6 +9725,7 @@ class UnparsedAbideConfig(object):
             "api_roots": self.api_roots,
         }
         d["authz_rules"] = self.authz_rules
+        d["authz_roles"] = self.authz_roles
         return d
 
     @classmethod
@@ -9732,6 +9737,7 @@ class UnparsedAbideConfig(object):
         unparsed_abide.authz_rules = data.get('authz_rules',
                                               data.get('admin_rules',
                                                        []))
+        unparsed_abide.authz_roles = data.get('authz_roles', [])
         unparsed_abide.semaphores = data.get("semaphores", [])
         unparsed_abide.api_roots = data.get("api_roots", [])
         return unparsed_abide
@@ -11092,6 +11098,7 @@ class TenantTPCRegistry:
 class Abide(object):
     def __init__(self):
         self.authz_rules = {}
+        self.authz_roles = {}
         self.semaphores = {}
         self.tenants = {}
         self.tenant_lock = threading.Lock()
@@ -11474,6 +11481,30 @@ class AuthZRuleTree(object):
 
     def __repr__(self):
         return '<AuthZRuleTree [ %s ]>' % self.ruletree
+
+
+class AuthZRole(object):
+    # TODO Build out an object that when given an explicit permission
+    # like enqueue or build etc checks if the role state matches. The
+    # primary check is the permission name. but if the AuthZRole includes
+    # conditions it will check each condition against the current request
+    # state.
+    pass
+
+
+class AuthZAdminRole(AuthZRole):
+    # Special role that always returns true to checks to enable admin action
+    pass
+
+
+class AuthZReadRole(AuthZRole):
+    # Special role that limits access to read only endpoints
+    pass
+
+
+class AuthZConfigRole(AuthZRole):
+    # Roles that are configured in the zuul config
+    pass
 
 
 class TenantEventState(zkobject.ZKObject):
